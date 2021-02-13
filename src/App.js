b71c679a139d49f6bc7abb0eab048254e408a91b
yogeshu <yogeshbhavsar1994@gmail.com>
2021-02-13 18:16:36 +0530

    added wordCount function to get acces of text area data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react"
 function App() {
+// Initialize The state that will hold typing words data and
+const [text,setText] = React.useState("");
+
+function wordCount (e){
+ const val = e.target.value;
+ setText(val)
+ console.log(val)
+}
+
   return (
     <div className="App">
     <h1> How Fast You can Type ? 
 
     </h1>
     <hr/>
-    <textarea />
+    <textarea onChange={wordCount} value={text} />
     <hr/>
     <h5>Time Left: Time</h5> 
     <button  > Start Game!</button>
