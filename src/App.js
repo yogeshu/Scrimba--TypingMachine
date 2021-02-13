ab5c1a0a60b79a958b81e449fba6e3c2ecde7741
yogeshu <yogeshbhavsar1994@gmail.com>
2021-02-13 18:12:15 +0530

    added html structure for game!

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1> How Fast You can Type ?
-      
+    <h1> How Fast You can Type ? 
+
     </h1>
+    <hr/>
+    <textarea />
+    <hr/>
+    <h5>Time Left: Time</h5> 
+    <button  > Start Game!</button>
+    <h5> Word Count</h5>
     </div>
   );
 }
