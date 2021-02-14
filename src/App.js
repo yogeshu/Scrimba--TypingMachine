1a327beaaff2fd70447b1129edfa13fa3a459375
yogeshu <yogeshbhavsar1994@gmail.com>
2021-02-14 11:15:44 +0530

    added focus on textarea

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const [text,setText] = React.useState("");
 const [count , setCount] = React.useState(0)
 const [timeLeft,setTimeLeft] = React.useState(6);
 const [isTimeRunning,setTimeRunning] = React.useState(false)
-
+const usetextAccesRef = React.useRef(null)
 function wordCount (e){
  const val = e.target.value;
  setText(val)
@@ -14,24 +14,29 @@ function wordCount (e){
 }
 function start(){
   setTimeRunning(true);
+  setTimeLeft(5)
+  usetextAccesRef.current.disabled = false;
+  usetextAccesRef.current.focus();
 }
 function end(){
   setTimeRunning(false)
+
+  setCount(countTheWord(text));
+
 }
 React.useEffect(()=> {
   if( isTimeRunning && timeLeft > 0){
 setTimeout(()=>{
   setTimeLeft(time => time - 1 )
 },
-1000)}
-else if(
+1000)} else if(timeLeft === 0)(
  end()
 )
   
 },[timeLeft,isTimeRunning])
 function countTheWord(){
- const word = text.trim().split(" ");
- const filterdata = word.filter(word => word !== " ");
+ const wArray = text.trim().split(" ");
+ const filterdata = wArray.filter(word => (word !== ""));
  return filterdata.length;
 }
 console.log(countTheWord())
@@ -41,10 +46,10 @@ console.log(countTheWord())
 
     </h1>
     <hr/>
-    <textarea onChange={wordCount} value={text}  disabled={!isTimeRunning}/>
+    <textarea onChange={wordCount}  value={text} ref={usetextAccesRef}  disabled={!isTimeRunning}/>
     <hr/>
     <h5>Time Left: {timeLeft }</h5> 
-    <button onClick={start} > Start Game!</button>
+    <button onClick={start} disabled={isTimeRunning}> Start Game!</button>
     <h5> Word Count : {count}</h5>
     </div>
   );
